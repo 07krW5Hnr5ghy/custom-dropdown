166129f51805ade958b563abab799daec97ea467
07krW5Hnr5ghy <jryn145@gmail.com>
2024-12-07 20:20:55 -0500

    feature(main): write logic for custom dropdown

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 const dropdownDefault = document.querySelector(".dropdown-default");
 const dropdownContent = document.querySelector(".dropdown-content");
+function cleanSignal(){
+    dropdownContent.childNodes.forEach((dropDownItem)=>{
+        console.log(dropDownItem.childElementCount);
+        if(dropDownItem.childElementCount===2){
+            dropDownItem.removeChild(dropDownItem.lastChild);
+        }
+    });
+}
 dropdownDefault.addEventListener("click",()=>{
     if(dropdownContent.style.display === "none"){
         dropdownContent.style.display = "flex";
@@ -8,4 +16,13 @@ dropdownDefault.addEventListener("click",()=>{
         dropdownContent.style.display = "none";
         dropdownDefault.textContent = "Select an Item v";
     }
+});
+dropdownContent.childNodes.forEach(dropDownItem => {
+    const signalCheck = document.createElement("span");
+    signalCheck.textContent = "x";
+    signalCheck.classList.add("signal-check");
+    dropDownItem.addEventListener("click",()=>{
+        cleanSignal();
+        dropDownItem.appendChild(signalCheck);
+    });
 });
